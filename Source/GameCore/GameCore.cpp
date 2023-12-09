9d503e8547c54a34218a406181fbd58cd4781dfe
Tayzar Linn <sueki.tayzar@gmail.com>
2023-12-09 16:17:37 -0500

    Move the super secret AES keys out and include these in public source

---
+++ Source/GameCore/GameCore.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) Tayzar Linn. All Rights Reserved.
+
+#include "GameCore.h"
+
+#define LOCTEXT_NAMESPACE "FGameCore"
+
+void FGameCoreModule::StartupModule()
+{
+}
+
+void FGameCoreModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FGameCoreModule, GameCore, "FaF_Rev");
