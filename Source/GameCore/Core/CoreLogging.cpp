b09fb5c83f564d987bc933b38db77299d953e011
Tayzar Linn <sueki.tayzar@gmail.com>
2023-12-09 21:16:21 -0500

    Initial commit

--- Source/GameCore/Core/CoreLogging.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (C) Tayzar (RedCraft86). All Rights Reserved.
-
-#include "CoreLogging.h"
-
-DEFINE_LOG_CATEGORY(GameCore)
-DEFINE_LOG_CATEGORY(UserSettings)
-DEFINE_LOG_CATEGORY(GameSequence)
--- Source/GameCore/Core/CoreLogging.h
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (C) Tayzar (RedCraft86). All Rights Reserved.
-
-#pragma once
-
-DECLARE_LOG_CATEGORY_EXTERN(GameCore, All, All)
-DECLARE_LOG_CATEGORY_EXTERN(UserSettings, All, All)
-DECLARE_LOG_CATEGORY_EXTERN(GameSequence, All, All)
