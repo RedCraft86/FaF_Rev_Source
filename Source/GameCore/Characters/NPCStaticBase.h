63eae6eec575212e507cf641920132f0da3094e5
Tayzar Linn <sueki.tayzar@gmail.com>
2023-12-16 22:39:00 -0500

    Add interaction data

--- Source/GameCore/Characters/NPCStaticBase.h
+++ Source/GameCore/Characters/NPCStaticBase.h
@@ -17,10 +17,10 @@ public:
 
 	ANPCStaticBase();
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DefaultSubobjects")
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "DefaultSubobjects")
 		class UCapsuleComponent* CapsuleComponent;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DefaultSubobjects")
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "DefaultSubobjects")
 		USkeletalMeshComponent* MeshComponent;
 	
 	UPROPERTY(VisibleDefaultsOnly, Category = "DefaultSubobjects")
@@ -29,23 +29,33 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "NPC")
 		USceneComponent* LookAtComponent;
 		
-	UPROPERTY(VisibleDefaultsOnly, Category = "NPC", meta = (GetOptions = "SocketNames"))
+	UPROPERTY(EditAnywhere, Category = "NPC", AdvancedDisplay, meta = (GetOptions = "SocketNames"))
 		FName HeadSocketName;
-
-	UPROPERTY(VisibleDefaultsOnly, Category = "NPC", meta = (MakeEditWidget = true))
+	
+	UPROPERTY(EditAnywhere, Category = "NPC", AdvancedDisplay, meta = (MakeEditWidget = true))
 		FVector LookAtLocation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "NPC")
+		FText CharacterName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NPC")
+		bool bCanInteract;
 
 	UFUNCTION(BlueprintPure, Category = "NPC")
 		void GetPlayerCameraInfo(float& AngleTo, FVector& Location) const;
-	
+
+	virtual bool GetInteractionInfo_Implementation(FText& DisplayName) override;
 	virtual FVector GetEyeWorldLocation_Implementation() override { return EyePosition->GetComponentLocation(); }
 	virtual FVector GetEyeForwardVector_Implementation() override { return EyePosition->GetForwardVector(); }
-	virtual USceneComponent* GetLookAtComponent_Implementation() override { return LookAtComponent; } 
+	virtual USceneComponent* GetLookAtComponent_Implementation() override { return LookAtComponent; }
+
+	virtual void GetSimpleCollisionCylinder(float& CollisionRadius, float& CollisionHalfHeight) const override;
 
 private:
 #if WITH_EDITORONLY_DATA
 	UPROPERTY(meta = (TransientToolProperty)) TArray<FName> SocketNames = {};
+	UPROPERTY(Transient) bool bAlreadySpawned = false;
 #endif
-
-	void OnConstruction(const FTransform& Transform) override;
+	
+	virtual void OnConstruction(const FTransform& Transform) override;
 };
