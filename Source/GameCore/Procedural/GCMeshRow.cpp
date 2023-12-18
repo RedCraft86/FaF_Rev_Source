248cb68ebb5b4142af40590af64ed911e0d49826
Tayzar Linn <sueki.tayzar@gmail.com>
2023-12-17 20:42:36 -0500

    New generator type!

---
+++ Source/GameCore/Procedural/GCMeshRow.cpp
@@ -0,0 +1,46 @@
+﻿// Copyright (C) Tayzar (RedCraft86). All Rights Reserved.
+
+#include "GCMeshRow.h"
+#include "RCRuntimeLibrary.h"
+
+AGCMeshRow::AGCMeshRow()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SetHidden(false);
+#if WITH_EDITORONLY_DATA
+	SceneRoot->bVisualizeComponent = true;
+#endif
+
+	Collision.SetCollisionProfile("BlockAllDynamic");
+	MeshCount = 1;
+	MeshOffset = FVector{10.0f, 0.0f, 0.0f};
+	InstanceTransform = FTransform::Identity;
+}
+
+void AGCMeshRow::Construct()
+{
+	StaticMeshes.Empty(MeshCount);
+	for (int i = 0; i < MeshCount; i++)
+	{
+		if (UStaticMeshComponent* MeshComp = NewObject<UStaticMeshComponent>(this))
+		{
+			MeshComp->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+			MeshComp->OnComponentCreated();
+			MeshComp->RegisterComponent();
+			MeshComp->SetRelativeTransform(FTransform{
+				InstanceTransform.Rotator(),
+				InstanceTransform.GetTranslation() + (MeshOffset * i),
+				InstanceTransform.GetScale3D()
+			});
+			
+			URCRuntimeLibrary::SetStaticMeshProperties(MeshComp, MeshData);
+			URCRuntimeLibrary::SetPrimitiveCollision(MeshComp, Collision);
+
+			MeshComp->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+			StaticMeshes.Add(MeshComp);
+		}
+	}
+	
+	Super::Construct();
+}
---
+++ Source/GameCore/Procedural/GCMeshRow.h
@@ -0,0 +1,49 @@
+﻿// Copyright (C) Tayzar (RedCraft86). All Rights Reserved.
+
+#pragma once
+
+#include "GCProceduralBase.h"
+#include "GCMeshRow.generated.h"
+
+UCLASS(NotBlueprintable, DisplayName = "Mesh Row")
+class GAMECORE_API AGCMeshRow final : public AGCProceduralBase
+{
+	GENERATED_BODY()
+
+public:
+
+	AGCMeshRow();
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FRCStaticMeshProperties MeshData;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FRCPrimitiveCollision Collision;
+
+	UPROPERTY(EditAnywhere, Category = "Settings", meta = (ClampMin = 1, UIMin = 1))
+		uint8 MeshCount;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FVector MeshOffset;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FTransform InstanceTransform;
+
+	virtual void Construct() override;
+
+private:
+
+	UPROPERTY(Transient)
+		TArray<UStaticMeshComponent*> StaticMeshes;
+
+#if WITH_EDITOR
+	virtual void OnConstruction(const FTransform& Transform) override
+	{
+		if (MeshData.Mesh)
+		{
+			MeshData.FillMaterials();
+		}
+		Super::OnConstruction(Transform);
+	}
+#endif
+};
