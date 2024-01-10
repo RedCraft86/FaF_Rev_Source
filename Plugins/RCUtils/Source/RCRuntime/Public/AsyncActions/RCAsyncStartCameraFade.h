80b53e1f49421fd9a9fefebc2bd24e49ef94b6fe
Tayzar Linn <sueki.tayzar@gmail.com>
2024-01-10 16:56:21 -0500

    Fix comment

--- Plugins/RCUtils/Source/RCRuntime/Public/AsyncActions/RCAsyncStartCameraFade.h
+++ Plugins/RCUtils/Source/RCRuntime/Public/AsyncActions/RCAsyncStartCameraFade.h
@@ -51,10 +51,13 @@ public: // Static functions (shortcuts)
 	UFUNCTION(BlueprintCallable, Category = "Camera Fades", meta = (WorldContext = "WorldContextObject", Keywords = "Fade Camera"))
 		static void SetCameraFade(const UObject* WorldContextObject, const float Alpha, const FLinearColor Color, const bool bFadeAudio);
 
-	/* Turns on camera fading at the given opacity. Does not auto-animate, allowing user to animate themselves.
-	* @param Alpha - Alpha/Amount to set the fade to.
-	* @param Color - Color of the fade.
-	* @param bFadeAudio - True to fade audio volume along with the alpha of the solid color.
+	/* Does a camera fade to/from a solid color. Animates automatically.
+	* @param FromAlpha - Alpha at which to begin the fade. Range [0..1], where 0 is fully transparent and 1 is fully opaque solid color.
+	* @param ToAlpha - Alpha at which to finish the fade.
+	* @param Duration - How long the fade should take, in seconds.
+	* @param Color - Color to fade to/from.
+	* @param bShouldFadeAudio - True to fade audio volume along with the alpha of the solid color.
+	* @param bHoldWhenFinished - True for fade to hold at the ToAlpha until explicitly stopped (e.g. with StopCameraFade).
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Camera Fades", meta = (WorldContext = "WorldContextObject", Keywords = "Fade Camera", AdvancedDisplay = "Color, bHoldWhenFinished"))
 		static void StartCameraFade(UObject* WorldContextObject, const float FromAlpha, const float ToAlpha = 1.0f, const float Duration = 1.0f, const FLinearColor Color = FLinearColor::Black, const bool bShouldFadeAudio = true, const bool bHoldWhenFinished = true);
