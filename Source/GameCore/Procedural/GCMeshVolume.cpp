2714ac57dcb16489bded3b5f1cfa2ca5cf8db4e8
Tayzar Linn <sueki.tayzar@gmail.com>
2024-01-11 18:06:58 -0500

    Mesh Scatter Volume

---
+++ Source/GameCore/Procedural/GCMeshVolume.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright (C) RedCraft86 (Tayzar). All Rights Reserved.
+
+#include "GCMeshVolume.h"
+#include "RCRuntimeLibrary.h"
+#include "Components/BoxComponent.h"
+#include "Components/InstancedStaticMeshComponent.h"
+
+AGCMeshVolume::AGCMeshVolume()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+	
+	BoundingBox = CreateDefaultSubobject<UBoxComponent>("CollisionBox");
+	BoundingBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	BoundingBox->SetCollisionObjectType(ECC_WorldDynamic);
+	BoundingBox->SetupAttachment(GetRootComponent());
+
+	InstancedMeshes = CreateDefaultSubobject<UInstancedStaticMeshComponent>("StaticMeshes");
+	BoundingBox->SetupAttachment(GetRootComponent());
+
+#if WITH_EDITORONLY_DATA
+	BoundingBox->SetLineThickness(0.5f);
+	SceneRoot->bVisualizeComponent = true;
+#endif
+
+	MeshCount = 1;
+	MeshTransform = FTransform::Identity;
+	Collision.SetCollisionProfile("NoCollision");
+}
+
+void AGCMeshVolume::Construct()
+{
+	InstancedMeshes->ClearInstances();
+	URCRuntimeLibrary::SetStaticMeshProperties(InstancedMeshes, MeshData);
+	URCRuntimeLibrary::SetPrimitiveCollision(InstancedMeshes, Collision);
+	for (int i = 0; i < MeshCount; i++)
+	{
+		const FVector RandomLocation = FMath::RandPointInBox(BoundingBox->Bounds.GetBox());
+		InstancedMeshes->AddInstance({
+			MeshTransform.GetRotation(),
+			MeshTransform.GetTranslation() + RandomLocation,
+			MeshTransform.GetScale3D()
+		});
+	}
+}
---
+++ Source/GameCore/Procedural/GCMeshVolume.h
@@ -0,0 +1,52 @@
+﻿// Copyright (C) RedCraft86 (Tayzar). All Rights Reserved.
+
+#pragma once
+
+#include "GCProceduralBase.h"
+#include "GCMeshVolume.generated.h"
+
+UCLASS(NotBlueprintable, DisplayName = "Mesh Scatter Volume")
+class GAMECORE_API AGCMeshVolume final : public AGCProceduralBase
+{
+	GENERATED_BODY()
+
+public:
+	
+	AGCMeshVolume();
+	
+	UPROPERTY(/*VisibleDefaultsOnly, */BlueprintReadOnly, Category = "DefaultSubobjects")
+		USceneComponent* SceneRoot;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "DefaultSubobjects")
+		class UBoxComponent* BoundingBox;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "DefaultSubobjects")
+		UInstancedStaticMeshComponent* InstancedMeshes;
+
+	UPROPERTY(EditAnywhere, Category = "Settings", meta = (ClampMin = 1, UIMin = 1))
+		int32 MeshCount;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	FTransform MeshTransform;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FRCStaticMeshProperties MeshData;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FRCPrimitiveCollision Collision;
+
+	virtual void Construct() override;
+	
+private:
+
+#if WITH_EDITOR
+	virtual void OnConstruction(const FTransform& Transform) override
+	{
+		if (MeshData.Mesh)
+		{
+			MeshData.FillMaterials();
+		}
+		Super::OnConstruction(Transform);
+	}
+#endif
+};
