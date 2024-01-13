dae5dc84d72dce397b84f221c3bbba9e8ecdfae7
Tayzar Linn <sueki.tayzar@gmail.com>
2024-01-12 19:29:43 -0500

    nvm, revert this

--- Source/GameCore/Procedural/GCProceduralBase.h
+++ Source/GameCore/Procedural/GCProceduralBase.h
@@ -82,7 +82,7 @@ public:
 		static void MakeStaticMesh() { FGlobalTabmanager::Get()->TryInvokeTab(FName("MergeActors")); }
 #endif
 
-private:
+protected:
 	
 	virtual void OnConstruction(const FTransform& Transform) override;
 };
