46260fdc07c98595530a03384ffb830fda55fd7b
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2024-03-21 21:21:25 -0400

    Delete Source directory

--- Source/GameCore/Core/CoreLogging.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (C) RedCraft86 (Tayzar). All Rights Reserved.
-
-#include "CoreLogging.h"
-
-DEFINE_LOG_CATEGORY(GameCore)
-DEFINE_LOG_CATEGORY(UserSettings)
-DEFINE_LOG_CATEGORY(GameSequence)
--- Source/GameCore/Core/CoreLogging.h
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (C) RedCraft86 (Tayzar). All Rights Reserved.
-
-#pragma once
-
-DECLARE_LOG_CATEGORY_EXTERN(GameCore, All, All)
-DECLARE_LOG_CATEGORY_EXTERN(UserSettings, All, All)
-DECLARE_LOG_CATEGORY_EXTERN(GameSequence, All, All)
