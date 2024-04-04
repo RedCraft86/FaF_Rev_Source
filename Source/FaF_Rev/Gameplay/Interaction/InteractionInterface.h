695e063854007ebe39a281bd299fd2c1bf773c24
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-04 19:29:10 -0400

    Initial

---
+++ Source/FaF_Rev/Gameplay/Interaction/InteractionInterface.h
@@ -0,0 +1,21 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "InteractionInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UInteractionInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class FAF_REV_API IInteractionInterface
+{
+	GENERATED_BODY()
+
+public:
+};
