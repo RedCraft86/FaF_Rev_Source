6f83417885b3286807908ef4dfd7a79c655e856a
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-04 18:13:41 -0400

    Game section manager initial

---
+++ Source/FaF_Rev/GameSection/GameSectionManager.h
@@ -0,0 +1,12 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameSection/GameSectionManagerBase.h"
+#include "GameSectionManager.generated.h"
+
+UCLASS()
+class FAF_REV_API UGameSectionManager final : public UGameSectionManagerBase
+{
+	GENERATED_BODY()
+};
