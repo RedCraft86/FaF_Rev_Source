eba674bd925e33f51ff0baf72702eb219ef48e66
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-04 18:12:26 -0400

    Inventory initial

---
+++ Source/FaF_Rev/Gameplay/Inventory/InventoryComponent.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "InventoryComponent.h"
+
+UInventoryComponent::UInventoryComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+}
---
+++ Source/FaF_Rev/Gameplay/Inventory/InventoryComponent.h
@@ -0,0 +1,16 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Inventory/InventoryComponentBase.h"
+#include "InventoryComponent.generated.h"
+
+UCLASS(ClassGroup = (Game), meta = (BlueprintSpawnableComponent))
+class FAF_REV_API UInventoryComponent final : public UInventoryComponentBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UInventoryComponent();
+};
