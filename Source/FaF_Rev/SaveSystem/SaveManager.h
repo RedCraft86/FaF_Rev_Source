b83b5e143f1f33671191df746c085f6cfcf073ac
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-04 18:12:32 -0400

    Save initial

---
+++ Source/FaF_Rev/SaveSystem/SaveManager.h
@@ -0,0 +1,12 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SaveManager.generated.h"
+
+UCLASS()
+class FAF_REV_API USaveManager final : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+};
