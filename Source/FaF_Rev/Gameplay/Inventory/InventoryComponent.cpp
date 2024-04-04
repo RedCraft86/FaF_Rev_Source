06ff72a9f3931bca9191c3f2f5c25ad6c74dd9f7
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-04 18:52:46 -0400

    Revert for cleanup

--- Source/FaF_Rev/Gameplay/Inventory/InventoryComponent.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (C) RedCraft86. All Rights Reserved.
-
-#include "InventoryComponent.h"
-
-UInventoryComponent::UInventoryComponent()
-{
-	PrimaryComponentTick.bCanEverTick = false;
-
-}
