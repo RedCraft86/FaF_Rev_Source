5b0125f7e378fa757affb93c208ac48dd9d696bd
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-04 21:28:08 -0400

    Minor change

--- Source/FaF_Rev/Gameplay/Interaction/Fields/TriggerField.cpp
+++ Source/FaF_Rev/Gameplay/Interaction/Fields/TriggerField.cpp
@@ -1,11 +1,13 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "TriggerField.h"
+#include "Components/BrushComponent.h"
 #include "FRPlayer.h"
 
 ATriggerField::ATriggerField()
 {
 	WorldEventComponent = CreateDefaultSubobject<UWorldEventComponent>("WorldEvents");
+	GetBrushComponent()->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 	bSingleUse = true;
 }
 
