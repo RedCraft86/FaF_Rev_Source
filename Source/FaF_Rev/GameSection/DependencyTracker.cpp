f4dc097f9af75c1a0d99cc5006fb43a39bc1ae19
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-08 18:35:31 -0400

    Improved dependency tracker

---
+++ Source/FaF_Rev/GameSection/DependencyTracker.cpp
@@ -0,0 +1,80 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "DependencyTracker.h"
+#include "AssetRegistry/IAssetRegistry.h"
+
+TSoftObjectPtr<UObject> UDependencyTracker::GetBaseObject(const TSubclassOf<UObject> AsType)
+{
+	return BaseObject;
+}
+
+TSet<FAssetData> UDependencyTracker::GetDependencies()
+{
+	TSet<FAssetData> Result;
+	for (const FName& Package : Dependencies)
+	{
+		TArray<FAssetData> Assets;
+		GetAssetRegistry()->GetAssetsByPackageName(Package, Assets);
+		if (!Assets.IsEmpty() && !Assets[0].IsAssetLoaded())
+		{
+			Result.Add(Assets[0]);
+		}
+	}
+
+	return Result;
+}
+
+IAssetRegistry* UDependencyTracker::GetAssetRegistry()
+{
+	static IAssetRegistry* AssetRegistry;
+	if (!AssetRegistry) AssetRegistry = IAssetRegistry::Get();
+	return AssetRegistry;
+}
+
+TSet<FName> UDependencyTracker::GetAllDependencies(const FName& BasePackage)
+{
+	TSet<FName> Result;
+	
+	TArray<FName> BasePackages;
+	GetAssetRegistry()->GetDependencies(BasePackage, BasePackages);
+	BasePackages.RemoveAll([](const FName& Package) -> bool
+	{
+		return !Package.ToString().StartsWith(TEXT("/Game/"));
+	});
+
+	uint8 Depth = 3;
+	TSet<FName> Packages = TSet(BasePackages);
+	while (Depth)
+	{
+		Depth--;
+		TSet<FName> SubPackages;
+		for (const FName& Package : Packages)
+		{
+			TArray<FName> TempPackages;
+			GetAssetRegistry()->GetDependencies(Package, TempPackages);
+			TempPackages.RemoveAll([](const FName& InPackage) -> bool
+			{
+				return !InPackage.ToString().StartsWith(TEXT("/Game/"));
+			});
+			SubPackages.Append(TempPackages);
+		}
+
+		Packages = SubPackages;
+		Result.Append(SubPackages);
+	}
+
+	Result.Add(BasePackage);
+	return Result;
+}
+#if WITH_EDITOR
+void UDependencyTracker::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+#if WITH_EDITORONLY_DATA
+	if (bRefresh || BaseObject != CachedObject)
+	{
+		Dependencies = GetAllDependencies(*BaseObject.GetLongPackageName());
+	}
+#endif
+}
+#endif
---
+++ Source/FaF_Rev/GameSection/DependencyTracker.h
@@ -0,0 +1,43 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/DataAsset.h"
+#include "DependencyTracker.generated.h"
+
+struct FAssetData;
+class IAssetRegistry;
+
+UCLASS(BlueprintType)
+class FAF_REV_API UDependencyTracker final : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(EditAnywhere, Category = "Tracker")
+		TSoftObjectPtr<UObject> BaseObject;
+
+	UPROPERTY(EditAnywhere, Category = "Tracker")
+		TSet<FName> Dependencies;
+
+	UFUNCTION(BlueprintPure, Category = "DependencyTracker", meta = (DynamicOutputParam = "ReturnValue", DeterminesOutputType = "AsType"))
+		TSoftObjectPtr<UObject> GetBaseObject(const TSubclassOf<UObject> AsType = UWorld::StaticClass());
+	
+	UFUNCTION(BlueprintPure, Category = "DependencyTracker")
+		TSet<FAssetData> GetDependencies();
+	
+	static IAssetRegistry* GetAssetRegistry();
+	static TSet<FName> GetAllDependencies(const FName& BasePackage);
+
+#if WITH_EDITORONLY_DATA
+private:
+	UPROPERTY(EditAnywhere, Category = "Tracker", meta = (DisplayPriority = -1))
+		bool bRefresh = false;
+#endif
+#if WITH_EDITOR
+private:
+	TSoftObjectPtr<UWorld> CachedObject = nullptr;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+};
