a60c7d8a7787b3125ebe26870620bce9a107a31c
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-11 18:15:59 -0400

    Project settings

---
+++ Source/FaF_Rev/Framework/FRSettings.h
@@ -0,0 +1,27 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/DeveloperSettings.h"
+#include "Asset/ExpressiveTextFields.h"
+#include "FRSettings.generated.h"
+
+UCLASS(Config = Game, DefaultConfig, DisplayName = "Game Project")
+class FAF_REV_API UFRSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFRSettings()
+	{
+		CategoryName = TEXT("Project");
+		SectionName = TEXT("Game Project");
+	}
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Defaults")
+		FExpressiveTextFields DefaultExpressiveTextFields;
+	
+	static UFRSettings* Get() { return GetMutableDefault<UFRSettings>(); }
+	static const UFRSettings* GetConst() { return GetDefault<UFRSettings>(); }
+};
