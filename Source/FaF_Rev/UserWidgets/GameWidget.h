87e78d3d75cfb91a2fb2bb401a1d3a48e1753df1
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-17 19:08:48 -0400

    Move messaging widgets to it's own class

--- Source/FaF_Rev/UserWidgets/GameWidget.h
+++ Source/FaF_Rev/UserWidgets/GameWidget.h
@@ -5,16 +5,11 @@
 #include "GTUserWidget.h"
 #include "GameWidget.generated.h"
 
-class UExprTextBlock;
-
 UCLASS(Abstract)
 class FAF_REV_API UGameWidgetBase final : public UGTUserWidget
 {
 	GENERATED_BODY()
 
 public:
-
-	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Elements|SmallNotice", meta = (BindWidget))
-		UExprTextBlock* SmallNoticeLabel;
-		
+	
 };
