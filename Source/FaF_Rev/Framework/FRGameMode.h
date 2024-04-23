1860c784c1d30ab536fec54eb449b7cd0fc039a9
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-23 18:14:22 -0400

    Move widget manager

--- Source/FaF_Rev/Framework/FRGameMode.h
+++ Source/FaF_Rev/Framework/FRGameMode.h
@@ -15,25 +15,4 @@ class FAF_REV_API AFRGameModeBase final : public AGTGameMode
 public:
 
 	AFRGameModeBase();
-
-	UPROPERTY(EditAnywhere, Category = "Settings")
-		TMap<TSubclassOf<UGTUserWidget>, FGameplayTagContainer> DefaultWidgets;
-
-	UFUNCTION(BlueprintCallable, Category = "GameMode", meta = (DynamicOutputParam = "ReturnValue", DeterminesOutputType = "Class"))
-		UGTUserWidget* FindOrAddWidget(const TSubclassOf<UGTUserWidget> Class, const FGameplayTagContainer InTags);
-	
-	UFUNCTION(BlueprintPure, Category = "GameMode", meta = (DynamicOutputParam = "ReturnValue", DeterminesOutputType = "Class"))
-		UGTUserWidget* GetWidget(const TSubclassOf<UGTUserWidget> Class, const FGameplayTag FilterTag);
-	
-	template<typename T>
-	T* GetWidget(const FGameplayTag FilterTag = {})
-	{
-		return Cast<T>(GetWidget(T::StaticClass(), FilterTag));
-	}
-
-private:
-
-	UPROPERTY(Transient) TMap<TSubclassOf<UGTUserWidget>, UGTUserWidget*> WidgetObjs;
-
-	virtual void BeginPlay() override;
 };
