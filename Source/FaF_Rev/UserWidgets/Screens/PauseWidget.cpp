954993b7010893cf21b005091558cae2917bf572
Tayzar Linn <sueki.tayzar@gmail.com>
2024-04-23 21:02:47 -0400

    Pause widget

---
+++ Source/FaF_Rev/UserWidgets/Screens/PauseWidget.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "PauseWidget.h"
+
+UPauseWidgetBase::UPauseWidgetBase(const FObjectInitializer& ObjectInitializer)
+{
+	ZOrder = 98;
+	bAutoAdd = false;
+}
---
+++ Source/FaF_Rev/UserWidgets/Screens/PauseWidget.h
@@ -0,0 +1,16 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GTUserWidget.h"
+#include "PauseWidget.generated.h"
+
+UCLASS(Abstract)
+class FAF_REV_API UPauseWidgetBase final : public UGTUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPauseWidgetBase(const FObjectInitializer& ObjectInitializer);
+};
