4fdf3c8a59d754db2c4c4e09b22123f9e844111d
Tayzar Linn <sueki.tayzar@gmail.com>
2024-05-07 17:12:02 -0400

    settings

---
+++ Source/FaF_Rev/GameSettings/GameSettingTypes.h
@@ -0,0 +1,39 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameSettingTypes.generated.h"
+
+UENUM(BlueprintType, DisplayName = "Color Blind Mode")
+enum class EFRColorBlind : uint8
+{
+	None = 0,
+	
+	// Deuteranope (red-green weak/blind)
+	Deuteranope = 1,
+	
+	// Protanope (red weak/blind)
+	Protanope = 2,
+	
+	// Tritanope (blue-yellow weak / bind)
+	Tritanope = 3
+};
+
+UENUM(BlueprintType, DisplayName = "Anti Aliasing Method")
+enum class EFRAntiAliasing : uint8
+{
+	FXAA	UMETA(DisplayName = "Fast Approximate Anti-Aliasing (FXAA)"),
+	TAA		UMETA(DisplayName = "Temporal Anti-Aliasing (TAA)"),
+	TSR		UMETA(DisplayName = "Temporal Super-Resolution (TSR)")
+};
+
+UENUM(BlueprintType, DisplayName = "Sound Type")
+enum class EFRSoundType : uint8
+{
+	Master,
+	Ambience,
+	Music,
+	SoundFX,
+	Voice
+};
+ENUM_RANGE_BY_FIRST_AND_LAST(EFRSoundType, EFRSoundType::Master, EFRSoundType::Voice);
---
+++ Source/FaF_Rev/GameSettings/GameSettings.h
@@ -0,0 +1,51 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameSettingTypes.h"
+#include "GameFramework/GameUserSettings.h"
+#include "GameSettings.generated.h"
+
+UCLASS(BlueprintType)
+class FAF_REV_API UGameSettings final : public UGameUserSettings
+{
+	GENERATED_BODY()
+
+public:
+
+	void SetShowFPS(const bool bShowFPS);
+	bool GetShowFPS() const { return bShowFPS; }
+
+	DECLARE_MULTICAST_DELEGATE_OneParam(FOnSettingsApply);
+	FOnSettingsApply OnDynamicApply, OnManualApply;
+
+private:
+	
+	UPROPERTY(Transient) TMap<EFRSoundType, USoundClass*> SoundTypeToClass;
+	UPROPERTY(Transient) class UFRGameInstance* GameInstance;
+	UPROPERTY(Transient) class USoundMix* SoundMixObject;
+
+// Configs
+	
+	UPROPERTY(Config) bool bShowFPS;
+	UPROPERTY(Config) bool bSmoothCamera;
+	UPROPERTY(Config) FVector2D SensitivityXY;
+	UPROPERTY(Config) uint8 FieldOfView;
+	
+	UPROPERTY(Config) uint8 Brightness;
+	UPROPERTY(Config) float Gamma;
+	
+	UPROPERTY(Config) EFRColorBlind ColorBlindMode;
+	UPROPERTY(Config) uint8 ColorBlindStrength;
+	
+	UPROPERTY(Config) bool bFancyBloom;
+	UPROPERTY(Config) uint8 MotionBlurAmount;
+	UPROPERTY(Config) EFRAntiAliasing AntiAliasingMethod;
+	
+	UPROPERTY(Config) TMap<EFRSoundType, uint8> SoundTypeToVolume;
+
+public: // Statics
+
+	static UGameSettings* Get() { return Cast<UGameSettings>(GEngine->GameUserSettings); }
+};
+ 
