5ad33e742008a98f3d627d53378cf325b077ef59
Tayzar Linn <sueki.tayzar@gmail.com>
2024-05-08 22:49:19 -0400

    subwidgets

---
+++ Source/FaF_Rev/UserWidgets/SubWidgets.cpp
@@ -0,0 +1,396 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "SubWidgets.h"
+
+#include "Blueprint/WidgetTree.h"
+#include "Components/Button.h"
+#include "Components/HorizontalBox.h"
+#include "Components/HorizontalBoxSlot.h"
+#include "Components/Image.h"
+#include "Components/SpinBox.h"
+#include "Components/TextBlock.h"
+
+void UFRAnimatedButtonBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	Button->OnClicked.AddDynamic(this, &UFRAnimatedButtonBase::OnButtonClicked);
+	Button->OnHovered.AddDynamic(this, &UFRAnimatedButtonBase::OnButtonHovered);
+	Button->OnUnhovered.AddDynamic(this, &UFRAnimatedButtonBase::OnButtonUnhovered);
+}
+
+void UFRAnimatedButtonBase::OnButtonClicked()
+{
+	OnClicked.Broadcast();
+	OnClickedBP.Broadcast();
+}
+
+void UFRAnimatedButtonBase::OnButtonHovered()
+{
+	PlayAnimationForward(ButtonAnim);
+}
+
+void UFRAnimatedButtonBase::OnButtonUnhovered()
+{
+	PlayAnimationReverse(ButtonAnim);
+}
+
+UFRKeybindRowBase::UFRKeybindRowBase(const FObjectInitializer& ObjectInitializer)
+	: UUserWidget(ObjectInitializer), LabelText(nullptr), KeybindsBox(nullptr)
+{
+	Label = FText::FromString("Keybind");
+	Keys = {};
+
+	DividerBrush.TintColor = FLinearColor::Gray;
+	DividerBrush.DrawAs = ESlateBrushDrawType::RoundedBox;
+	DividerBrush.ImageSize = FVector2D(4.0f, 32.0f);
+	DividerBrush.OutlineSettings.CornerRadii = FVector4(2.0f, 2.0f, 2.0f, 2.0f);
+	DividerBrush.OutlineSettings.RoundingType = ESlateBrushRoundingType::FixedRadius;
+	DividerBrush.OutlineSettings.bUseBrushTransparency = true;
+}
+
+void UFRKeybindRowBase::CreateIcons()
+{
+	if (KeybindsBox)
+	{
+		KeybindsBox->ClearChildren();
+		for (int32 i = 0; i < Keys.Num(); i++)
+		{
+			UImage* IconImage = WidgetTree->ConstructWidget<UImage>();
+			IconImage->SetBrush(GetIconForKey(Keys[i]));
+
+			UHorizontalBoxSlot* IconSlot = Cast<UHorizontalBoxSlot>(KeybindsBox->AddChild(IconImage));
+			IconSlot->SetHorizontalAlignment(HAlign_Center);
+			IconSlot->SetVerticalAlignment(VAlign_Center);
+
+			if (Keys.Num() != 1 && i != Keys.Num() - 1)
+			{
+				UImage* DividerImage = WidgetTree->ConstructWidget<UImage>();
+				DividerImage->SetBrush(DividerBrush);
+						
+				UHorizontalBoxSlot* DividerSlot = Cast<UHorizontalBoxSlot>(KeybindsBox->AddChild(DividerImage));
+				DividerSlot->SetPadding(FMargin(5.0f, 0.0f));
+				DividerSlot->SetHorizontalAlignment(HAlign_Center);
+				DividerSlot->SetVerticalAlignment(VAlign_Fill);
+			}
+		}
+	}
+}
+
+void UFRKeybindRowBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (LabelText) LabelText->SetText(Label);
+	CreateIcons();
+}
+
+void UFRSettingRowBase::CheckDefaultValue()
+{
+	ResetButton->SetVisibility(IsDefaultValue() ? ESlateVisibility::Hidden : ESlateVisibility::Visible);
+}
+
+void UFRSettingRowBase::NativeConstruct()
+{
+	Super::NativeConstruct();
+	CheckDefaultValue();
+}
+
+void UFRSettingRowBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	ResetButton->OnClicked.AddDynamic(this, &UFRSettingRowBase::OnResetClicked);
+}
+
+void UFRSettingRowBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (LabelText) LabelText->SetText(Label);
+}
+
+void UFRSettingRowBase::OnResetClicked()
+{
+	if (!IsDefaultValue()) ResetValue();
+}
+
+void UFRToggleSettingBase::SetValueSilently(const bool bInValue)
+{
+	bCurrentValue = bInValue;
+	ToggleLabelText->SetText(FText::FromName(bCurrentValue ? EnabledLabel : DisabledLabel));
+	ToggleLabelText->SetColorAndOpacity(bCurrentValue ? EnabledColor : DisabledColor);
+	CheckDefaultValue();
+}
+
+void UFRToggleSettingBase::SetValue(const bool bInValue)
+{
+	if (bCurrentValue != bInValue)
+	{
+		SetValueSilently(bInValue);
+		if (SetterFunction) SetterFunction(bCurrentValue);
+		
+		OnValueChanged.Broadcast(bCurrentValue);
+		OnValueChangedBP.Broadcast(bCurrentValue);
+	}
+}
+
+void UFRToggleSettingBase::ResetValue()
+{
+	SetValue(bDefaultValue);
+}
+
+void UFRToggleSettingBase::RefreshValue()
+{
+	if (GetterFunction) SetValueSilently(GetterFunction());
+}
+
+bool UFRToggleSettingBase::IsDefaultValue()
+{
+	return bCurrentValue == bDefaultValue;
+}
+
+void UFRToggleSettingBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	bCurrentValue = bDefaultValue;
+	ToggleLabelText->SetText(FText::FromName(bCurrentValue ? EnabledLabel : DisabledLabel));
+	ToggleLabelText->SetColorAndOpacity(bCurrentValue ? EnabledColor : DisabledColor);
+	ToggleButton->OnClicked.AddDynamic(this, &UFRToggleSettingBase::OnToggleButtonClicked);
+}
+
+#if WITH_EDITOR
+void UFRToggleSettingBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (!FApp::IsGame())
+	{
+		bCurrentValue = bDefaultValue;
+		if (ToggleLabelText)
+		{
+			ToggleLabelText->SetText(FText::FromName(bCurrentValue ? EnabledLabel : DisabledLabel));
+			ToggleLabelText->SetColorAndOpacity(bCurrentValue ? EnabledColor : DisabledColor);
+		}
+	}
+}
+#endif
+
+void UFRToggleSettingBase::OnToggleButtonClicked()
+{
+	SetValue(!bCurrentValue);
+}
+
+void UFRSliderSettingBase::SetValueSilently(const float InValue)
+{
+	bHaveDoneInit = false;
+	SliderSpinBox->SetValue(FMath::Clamp(InValue, MinValue, MaxValue));
+	CheckDefaultValue();
+	bHaveDoneInit = true;
+}
+
+void UFRSliderSettingBase::SetValue(const float InValue)
+{
+	const float ClampedValue = FMath::Clamp(InValue, MinValue, MaxValue);
+	if (SliderSpinBox->GetValue() != ClampedValue)
+	{
+		SliderSpinBox->SetValue(ClampedValue);
+		CheckDefaultValue();
+
+		if (!bBroadcastAnyChange)
+		{
+			BroadcastChange();
+		}
+	}
+}
+
+float UFRSliderSettingBase::GetValue() const
+{
+	return SliderSpinBox->GetValue();
+}
+
+void UFRSliderSettingBase::ResetValue()
+{
+	SetValue(DefaultValue);
+}
+
+void UFRSliderSettingBase::RefreshValue()
+{
+	if (GetterFunction) SetValueSilently(GetterFunction());
+}
+
+bool UFRSliderSettingBase::IsDefaultValue()
+{
+	return SliderSpinBox->GetValue() == DefaultValue;
+}
+
+void UFRSliderSettingBase::ApplySliderSettings() const
+{
+	SliderSpinBox->SetMinValue(MinValue);
+	SliderSpinBox->SetMaxValue(MaxValue);
+	SliderSpinBox->SetMinSliderValue(MinValue);
+	SliderSpinBox->SetMaxSliderValue(MaxValue);
+	SliderSpinBox->SetMaxFractionalDigits(NumOfDecimals);
+	SliderSpinBox->SetMinFractionalDigits(NumOfDecimals > 0 ? 1 : 0);
+	SliderSpinBox->SetDelta(NumOfDecimals > 0 ? 0.0f : 1.0f);
+}
+
+void UFRSliderSettingBase::NativeConstruct()
+{
+	Super::NativeConstruct();
+	bHaveDoneInit = true;
+}
+
+void UFRSliderSettingBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	ApplySliderSettings();
+	SliderSpinBox->SetValue(FMath::Clamp(DefaultValue, MinValue, MaxValue));
+	SliderSpinBox->OnValueChanged.AddDynamic(this, &UFRSliderSettingBase::OnSliderValueChanged);
+	SliderSpinBox->OnEndSliderMovement.AddDynamic(this, &UFRSliderSettingBase::OnSliderMovementEnd);
+	SliderSpinBox->OnValueCommitted.AddDynamic(this, &UFRSliderSettingBase::OnSliderValueCommitted);
+}
+
+#if WITH_EDITOR
+void UFRSliderSettingBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (!FApp::IsGame())
+	{
+		DefaultValue = FMath::Clamp(DefaultValue, MinValue, MaxValue);
+		if (SliderSpinBox)
+		{
+			ApplySliderSettings();
+			SliderSpinBox->SetValue(DefaultValue);
+		}
+	}
+}
+#endif
+
+void UFRSliderSettingBase::BroadcastChange() const
+{
+	const float Val = SliderSpinBox->GetValue();
+	if (SetterFunction) SetterFunction(Val);
+	
+	OnValueChanged.Broadcast(Val);
+	OnValueChangedBP.Broadcast(Val);
+}
+
+void UFRSliderSettingBase::OnSliderValueChanged(float Value)
+{
+	if (bHaveDoneInit && bBroadcastAnyChange)
+	{
+		CheckDefaultValue();
+		BroadcastChange();
+	}
+}
+
+void UFRSliderSettingBase::OnSliderMovementEnd(float Value)
+{
+	if (!bBroadcastAnyChange)
+	{
+		CheckDefaultValue();
+		BroadcastChange();
+	}
+}
+
+void UFRSliderSettingBase::OnSliderValueCommitted(float Value, ETextCommit::Type Type)
+{
+	if (!bBroadcastAnyChange)
+	{
+		CheckDefaultValue();
+		BroadcastChange();
+	}
+}
+
+void UFRSwitcherSettingBase::SetValueSilently(const int32 InValue)
+{
+	if (InValue >= 0 && ValueOptions.IsValidIndex(InValue))
+	{
+		CurrentIdx = InValue;
+		CheckDefaultValue();
+
+		const FName AsName = ValueOptions[CurrentIdx];
+		ValueText->SetText(FText::FromName(AsName));
+		OptionTooltip = OptionTooltips.FindRef(AsName);
+	}
+}
+
+void UFRSwitcherSettingBase::SetValue(const int32 InValue)
+{
+	if (CurrentIdx != InValue && InValue >= 0 && ValueOptions.IsValidIndex(InValue))
+	{
+		SetValueSilently(InValue);
+
+		if (SetterFunction) SetterFunction(CurrentIdx);
+		
+		const FName AsName = ValueOptions[CurrentIdx];
+		OnValueChanged.Broadcast(CurrentIdx, AsName);
+		OnValueChangedBP.Broadcast(CurrentIdx, AsName);
+	}
+}
+
+void UFRSwitcherSettingBase::ResetValue()
+{
+	SetValue(DefaultValue);
+}
+
+void UFRSwitcherSettingBase::RefreshValue()
+{
+	if (GetterFunction) SetValueSilently(GetterFunction());
+}
+
+bool UFRSwitcherSettingBase::IsDefaultValue()
+{
+	return CurrentIdx == DefaultValue;
+}
+
+void UFRSwitcherSettingBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	ValueOptions.Remove(NAME_None);
+	CurrentIdx = FMath::Clamp(DefaultValue, 0, ValueOptions.Num() - 1);
+	OptionTooltip = OptionTooltips.FindRef(ValueOptions[CurrentIdx]);
+	ValueText->SetText(FText::FromName(ValueOptions[CurrentIdx]));
+	
+	PrevButton->OnClicked.AddDynamic(this, &UFRSwitcherSettingBase::OnPrevClicked);
+	NextButton->OnClicked.AddDynamic(this, &UFRSwitcherSettingBase::OnNextClicked);
+}
+
+#if WITH_EDITOR
+void UFRSwitcherSettingBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (!FApp::IsGame())
+	{
+		FName TargetText = NAME_None;
+		if (ValueOptions.IsEmpty())
+		{
+			ValueOptions.Add(TEXT("Option"));
+			DefaultValue = 0;
+			CurrentIdx = 0;
+		}
+		else
+		{
+			DefaultValue = FMath::Clamp(DefaultValue, 0, ValueOptions.Num() - 1);
+			CurrentIdx = DefaultValue;
+			
+			TargetText = ValueOptions[CurrentIdx];
+		}
+
+		if (ValueText)
+		{
+			ValueText->SetText(FText::FromName(TargetText));
+		}
+	}
+}
+#endif
+
+void UFRSwitcherSettingBase::OnPrevClicked()
+{
+	int32 TargetIdx = CurrentIdx - 1;
+	if (TargetIdx < 0) TargetIdx = ValueOptions.Num() - 1;
+	SetValue(TargetIdx);
+}
+
+void UFRSwitcherSettingBase::OnNextClicked()
+{
+	int32 TargetIdx = CurrentIdx + 1;
+	if (TargetIdx >= ValueOptions.Num()) TargetIdx = 0;
+	SetValue(TargetIdx);
+}
---
+++ Source/FaF_Rev/UserWidgets/SubWidgets.h
@@ -0,0 +1,347 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "SubWidgets.generated.h"
+
+class UButton;
+class UTextBlock;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSimpleSubWidgetDelegateBP);
+
+UCLASS(Abstract, DisplayName = "Animated Button Base")
+class FAF_REV_API UFRAnimatedButtonBase final : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFRAnimatedButtonBase(const FObjectInitializer& ObjectInitializer)
+		: UUserWidget(ObjectInitializer), Button(nullptr), ButtonAnim(nullptr)
+	{}
+
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Clicked")
+		FSimpleSubWidgetDelegateBP OnClickedBP;
+	
+	UPROPERTY(Transient, meta = (BindWidget))
+		UButton* Button;
+
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+		UWidgetAnimation* ButtonAnim;
+
+	DECLARE_MULTICAST_DELEGATE(FOnClicked);
+	FOnClicked OnClicked;
+
+	virtual void NativeOnInitialized() override;
+	
+protected:
+
+	UFUNCTION()
+		void OnButtonClicked();
+
+	UFUNCTION()
+		void OnButtonHovered();
+
+	UFUNCTION()
+		void OnButtonUnhovered();
+};
+
+UCLASS(Abstract, DisplayName = "Keybind Row Base")
+class FAF_REV_API UFRKeybindRowBase final : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFRKeybindRowBase(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		UTextBlock* LabelText;
+	
+	UPROPERTY(Transient, meta = (BindWidget))
+		class UHorizontalBox* KeybindsBox;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FText Label;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		TArray<FKey> Keys;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FSlateBrush DividerBrush;
+
+	UFUNCTION(BlueprintImplementableEvent)
+		FSlateBrush GetIconForKey(const FKey& InKey);
+	
+protected:
+
+	void CreateIcons();
+	virtual void NativePreConstruct() override;
+};
+
+UCLASS(Abstract, NotBlueprintable, DisplayName = "Setting Row Base")
+class FAF_REV_API UFRSettingRowBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFRSettingRowBase(const FObjectInitializer& ObjectInitializer)
+		: UUserWidget(ObjectInitializer), LabelText(nullptr), ResetButton(nullptr), Label(INVTEXT("Setting"))
+	{}
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		UTextBlock* LabelText;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		UButton* ResetButton;
+
+	UPROPERTY(EditAnywhere, Category = "Settings", meta = (DisplayPriority = -1))
+		FText Label;
+
+	virtual void ResetValue() {}
+	virtual void RefreshValue() {}
+	virtual bool IsDefaultValue() { return true; }
+	
+protected:
+
+	void CheckDefaultValue();
+	virtual void NativeConstruct() override;
+	virtual void NativeOnInitialized() override;
+	virtual void NativePreConstruct() override;
+
+private:
+	
+	UFUNCTION()
+		void OnResetClicked();
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnToggleValueChanged, bool, bEnabled);
+
+UCLASS(Abstract, Blueprintable, DisplayName = "Toggle Setting Row Base")
+class FAF_REV_API UFRToggleSettingBase final : public UFRSettingRowBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFRToggleSettingBase(const FObjectInitializer& ObjectInitializer)
+		: UFRSettingRowBase(ObjectInitializer), ToggleButton(nullptr), ToggleLabelText(nullptr)
+		, bDefaultValue(false), EnabledLabel(TEXT("Enabled")), EnabledColor(FLinearColor::Yellow)
+		, DisabledLabel(TEXT("Disabled")), DisabledColor(FLinearColor::White), bCurrentValue(false)
+		, GetterFunction(nullptr), SetterFunction(nullptr)
+	{}
+
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Value Changed")
+		FOnToggleValueChanged OnValueChangedBP;
+	
+	UPROPERTY(Transient, meta = (BindWidget))
+		UButton* ToggleButton;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		UTextBlock* ToggleLabelText;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		bool bDefaultValue;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FName EnabledLabel;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FLinearColor EnabledColor;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FName DisabledLabel;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		FLinearColor DisabledColor;
+
+	UFUNCTION(BlueprintCallable, Category = "SettingRow|Toggle")
+		void SetValueSilently(const bool bInValue);
+	
+	UFUNCTION(BlueprintCallable, Category = "SettingRow|Toggle")
+		void SetValue(const bool bInValue);
+	
+	UFUNCTION(BlueprintPure, Category = "SettingRow|Toggle")
+		bool GetValue() const { return bCurrentValue; }
+	
+	DECLARE_MULTICAST_DELEGATE_OneParam(FOnValueChanged, bool);
+	FOnValueChanged OnValueChanged;
+	
+	virtual void ResetValue() override;
+	virtual void RefreshValue() override;
+	virtual bool IsDefaultValue() override;
+	void AssignGetterFunction(const TFunction<bool()>& Function) { if (Function) GetterFunction = Function; }
+	void AssignSetterFunction(const TFunction<void(const bool)>& Function) { if (Function) SetterFunction = Function; }
+	
+protected:
+	
+	bool bCurrentValue;
+	TFunction<bool()> GetterFunction;
+	TFunction<void(const bool)> SetterFunction;
+	
+	virtual void NativeOnInitialized() override;
+#if WITH_EDITOR
+	virtual void NativePreConstruct() override;
+#endif
+
+	UFUNCTION()
+		void OnToggleButtonClicked();
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSliderValueChanged, float, Value);
+
+UCLASS(Abstract, Blueprintable, DisplayName = "Slider Setting Row Base")
+class FAF_REV_API UFRSliderSettingBase final : public UFRSettingRowBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFRSliderSettingBase(const FObjectInitializer& ObjectInitializer)
+		: UFRSettingRowBase(ObjectInitializer), SliderSpinBox(nullptr), DefaultValue(0.0f), MinValue(0.0f), MaxValue(100.0f)
+		, NumOfDecimals(0), bBroadcastAnyChange(false), bHaveDoneInit(false), GetterFunction(nullptr), SetterFunction(nullptr)
+	{}
+
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Value Changed")
+		FOnSliderValueChanged OnValueChangedBP;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		class USpinBox* SliderSpinBox;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		float DefaultValue;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		float MinValue;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		float MaxValue;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		uint8 NumOfDecimals;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+		bool bBroadcastAnyChange;
+
+	UFUNCTION(BlueprintCallable, Category = "SettingRow|Slider")
+		void SetValueSilently(const float InValue);
+	
+	UFUNCTION(BlueprintCallable, Category = "SettingRow|Slider")
+		void SetValue(const float InValue);
+	
+	UFUNCTION(BlueprintPure, Category = "SettingRow|Slider")
+		float GetValue() const;
+	
+	DECLARE_MULTICAST_DELEGATE_OneParam(FOnValueChanged, float);
+	FOnValueChanged OnValueChanged;
+	
+	virtual void ResetValue() override;
+	virtual void RefreshValue() override;
+	virtual bool IsDefaultValue() override;
+	void AssignGetterFunction(const TFunction<float()>& Function) { if (Function) GetterFunction = Function; }
+	void AssignSetterFunction(const TFunction<void(const float)>& Function) { if (Function) SetterFunction = Function; }
+	
+protected:
+	
+	bool bHaveDoneInit;
+	TFunction<float()> GetterFunction;
+	TFunction<void(const float)> SetterFunction;
+	
+	void ApplySliderSettings() const;
+	virtual void NativeConstruct() override;
+	virtual void NativeOnInitialized() override;
+#if WITH_EDITOR
+	virtual void NativePreConstruct() override;
+#endif
+
+	void BroadcastChange() const;
+	
+	UFUNCTION()
+		void OnSliderValueChanged(float Value);
+
+	UFUNCTION()
+		void OnSliderMovementEnd(float Value);
+	
+	UFUNCTION()
+		void OnSliderValueCommitted(float Value, ETextCommit::Type Type);
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnSwitcherValueChanged, int32, Index, FName, Value);
+
+UCLASS(Abstract, Blueprintable, DisplayName = "Switcher Setting Row Base")
+class FAF_REV_API UFRSwitcherSettingBase final : public UFRSettingRowBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFRSwitcherSettingBase(const FObjectInitializer& ObjectInitializer)
+		: UFRSettingRowBase(ObjectInitializer), ValueText(nullptr), PrevButton(nullptr), NextButton(nullptr)
+		, DefaultValue(0), ValueOptions({}), OptionTooltips({}), OptionTooltip(FText::GetEmpty())
+		, CurrentIdx(0), GetterFunction(nullptr), SetterFunction(nullptr)
+	{}
+
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Value Changed")
+		FOnSwitcherValueChanged OnValueChangedBP;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		UTextBlock* ValueText;
+	
+	UPROPERTY(Transient, meta = (BindWidget))
+		UButton* PrevButton;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		UButton* NextButton;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		uint8 DefaultValue;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings", meta = (NoElementDuplicate))
+		TArray<FName> ValueOptions;
+
+	UPROPERTY(EditAnywhere, Category = "Settings", meta = (GetOptions = "ValueOptions", MultiLine = true))
+		TMap<FName, FText> OptionTooltips;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Settings")
+		FText OptionTooltip;
+	
+	UFUNCTION(BlueprintCallable, Category = "SettingRow|Switcher")
+		void SetValueSilently(const int32 InValue);
+	
+	UFUNCTION(BlueprintCallable, Category = "SettingRow|Switcher")
+		void SetValue(const int32 InValue);
+	
+	UFUNCTION(BlueprintPure, Category = "SettingRow|Switcher")
+		FName GetSelectedValue() const { return CurrentIdx != INDEX_NONE ? ValueOptions[CurrentIdx] : NAME_None; }
+
+	UFUNCTION(BlueprintPure, Category = "SettingRow|Switcher")
+		int32 GetSelectedIndex() const { return CurrentIdx; }
+	
+	DECLARE_MULTICAST_DELEGATE_TwoParams(FOnValueChanged, int32, FName);
+	FOnValueChanged OnValueChanged;
+	
+	virtual void ResetValue() override;
+	virtual void RefreshValue() override;
+	virtual bool IsDefaultValue() override;
+	void AssignGetterFunction(const TFunction<uint8()>& Function) { if (Function) GetterFunction = Function; }
+	void AssignSetterFunction(const TFunction<void(const uint8)>& Function) { if (Function) SetterFunction = Function; }
+	
+protected:
+	
+	uint8 CurrentIdx;
+	TFunction<uint8()> GetterFunction;
+	TFunction<void(const uint8)> SetterFunction;
+	
+	virtual void NativeOnInitialized() override;
+#if WITH_EDITOR
+	virtual void NativePreConstruct() override;
+#endif
+
+	UFUNCTION()
+		void OnPrevClicked();
+
+	UFUNCTION()
+		void OnNextClicked();
+};
