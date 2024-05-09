b360166d468749aa84d692af5e4203d28641dcd3
Tayzar Linn <sueki.tayzar@gmail.com>
2024-05-08 21:54:06 -0400

    settings widget base

---
+++ Source/FaF_Rev/UserWidgets/Screens/SettingsWidget.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "SettingsWidget.h"
+
+USettingsWidgetBase::USettingsWidgetBase(const FObjectInitializer& ObjectInitializer)
+	: UGTUserWidget(ObjectInitializer)
+{
+	ZOrder = 98;
+	bAutoAdd = false;
+}
---
+++ Source/FaF_Rev/UserWidgets/Screens/SettingsWidget.h
@@ -0,0 +1,16 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GTUserWidget.h"
+#include "SettingsWidget.generated.h"
+
+UCLASS(Abstract)
+class FAF_REV_API USettingsWidgetBase final : public UGTUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	USettingsWidgetBase(const FObjectInitializer& ObjectInitializer);
+};
