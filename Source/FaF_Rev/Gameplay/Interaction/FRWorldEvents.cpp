db1977a4c904968bc0c095f69fef5aba7e104436
Tayzar Linn <sueki.tayzar@gmail.com>
2024-05-10 21:35:54 -0400

    World events for narrative

---
+++ Source/FaF_Rev/Gameplay/Interaction/FRWorldEvents.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "FRWorldEvents.h"
+#include "NarrativeComponent.h"
+#include "FRGameMode.h"
+
+void FWENarrativeDialogue::RunEvent(const UObject* WorldContext)
+{
+	if (const AFRGameModeBase* GM = FRGameMode(WorldContext))
+	{
+		if (!GM->Narrative->IsInDialogue())
+			GM->Narrative->BeginDialogue(Dialogue.LoadSynchronous(), NAME_None);
+	}
+}
+
+void FWENarrativeQuest::RunEvent(const UObject* WorldContext)
+{
+	if (const AFRGameModeBase* GM = FRGameMode(WorldContext))
+	{
+		if (!GM->Narrative->IsQuestInProgress(Quest.LoadSynchronous()))
+			GM->Narrative->BeginQuest(Quest.LoadSynchronous(), NAME_None);
+	}
+}
+
+void FWENarrativeTask::RunEvent(const UObject* WorldContext)
+{
+	if (const AFRGameModeBase* GM = FRGameMode(WorldContext))
+	{
+		if (!Task.IsNull() && !Argument.IsEmpty() && Quantity > 0)
+			GM->Narrative->CompleteNarrativeDataTask(Task.LoadSynchronous(), Argument, Quantity);
+	}
+}
---
+++ Source/FaF_Rev/Gameplay/Interaction/FRWorldEvents.h
@@ -0,0 +1,60 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Quest.h"
+#include "Dialogue.h"
+#include "NarrativeDataTask.h"
+#include "WorldEvents/WorldEvents.h"
+#include "FRWorldEvents.generated.h"
+
+USTRUCT(BlueprintType, DisplayName = "Narrative Dialogue")
+struct FAF_REV_API FWENarrativeDialogue final : public FWorldEventBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue")
+		TSoftClassPtr<UDialogue> Dialogue;
+
+	FWENarrativeDialogue() : Dialogue(nullptr) {}
+	
+protected:
+
+	virtual void RunEvent(const UObject* WorldContext) override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Narrative Quest")
+struct FAF_REV_API FWENarrativeQuest final : public FWorldEventBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Quest")
+		TSoftClassPtr<UQuest> Quest;
+
+	FWENarrativeQuest() : Quest(nullptr) {}
+	
+protected:
+
+	virtual void RunEvent(const UObject* WorldContext) override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Narrative Task")
+struct FAF_REV_API FWENarrativeTask final : public FWorldEventBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Task")
+		TSoftObjectPtr<UNarrativeDataTask> Task;
+		
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Task")
+		FString Argument;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Task")
+		uint8 Quantity;
+	
+	FWENarrativeTask() : Task(nullptr), Argument(TEXT("")), Quantity(1) {}
+	
+protected:
+
+	virtual void RunEvent(const UObject* WorldContext) override;
+};
