3207cb16489b8747719f7821f666a603e842547c
Tayzar Linn <sueki.tayzar@gmail.com>
2024-06-16 20:55:24 -0400

    pulldowns

---
+++ Plugins/GameTemplatePlugin/Source/GTCore/Public/Data/PulldownTypes.h
@@ -0,0 +1,76 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "PulldownTypes.generated.h"
+
+USTRUCT()
+struct GTCORE_API FPulldownOption final
+{
+	GENERATED_BODY()
+#if WITH_EDITORONLY_DATA
+	UPROPERTY() FString Option;
+	UPROPERTY() FString Tooltip;
+	FPulldownOption() : Option(TEXT("")), Tooltip(TEXT("")) {}
+	FPulldownOption(const FString& InOption, const FString& InTooltip)
+		: Option(InOption), Tooltip(InTooltip) {}
+#else
+	FPulldownOption() {}
+#endif
+};
+
+USTRUCT(BlueprintInternalUseOnly)
+struct GTCORE_API FStringPulldown
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pulldown")
+		FString Value;
+
+	FStringPulldown() : Value(TEXT("")) {}
+	virtual ~FStringPulldown() = default;
+
+#if WITH_EDITORONLY_DATA
+	virtual TArray<FPulldownOption> GetPulldownOptions() { return {}; }
+#endif
+};
+
+USTRUCT(BlueprintInternalUseOnly)
+struct GTCORE_API FStringListPulldown : public FStringPulldown
+{
+	GENERATED_BODY()
+	
+#if WITH_EDITORONLY_DATA
+protected:
+	UPROPERTY(meta = (TransientToolProperty)) TArray<FPulldownOption> Options = {};
+	virtual TArray<FPulldownOption> GetPulldownOptions() override { return Options; }
+#endif
+};
+
+USTRUCT(BlueprintInternalUseOnly)
+struct GTCORE_API FNamePulldown
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pulldown")
+		FName Value;
+
+	FNamePulldown() : Value(NAME_None) {}
+	virtual ~FNamePulldown() = default;
+
+#if WITH_EDITORONLY_DATA
+	virtual TArray<FPulldownOption> GetPulldownOptions() { return {}; }
+#endif
+};
+
+USTRUCT(BlueprintInternalUseOnly)
+struct GTCORE_API FNameListPulldown : public FNamePulldown
+{
+	GENERATED_BODY()
+	
+#if WITH_EDITORONLY_DATA
+protected:
+	UPROPERTY(meta = (TransientToolProperty)) TArray<FPulldownOption> Options = {};
+	virtual TArray<FPulldownOption> GetPulldownOptions() override { return Options; }
+#endif
+};
